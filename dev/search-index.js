6ea1597c832b702148de1318fee3584e2ef24e11
bookworm <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-21 21:30:55 +0000

    Generate mdbook

---
+++ dev/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["darkfi_derive",{"doc":"","t":"YY","n":["SerialDecodable","SerialEncodable"],"q":[[0,"darkfi_derive"]],"d":["",""],"i":[0,0],"f":"``","c":[],"p":[],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ dev/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["darkfi_derive",["",[],["lib.rs"]]]\
+]'));
+createSrcSidebar();
